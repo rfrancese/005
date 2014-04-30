912b915e2a53953e3026747b8251d099a4fcd461
davide.scarano <davidescarano1991@gmail.com>
2014-04-30 12:40:17 +0200

    committ 30/04 by ds

--- aa/gen/com/example/aa/R.java
+++ aa/gen/com/example/aa/R.java
@@ -8,6 +8,10 @@
 package com.example.aa;
 
 public final class R {
+    public static final class array {
+        public static final int icons=0x7f060001;
+        public static final int titles=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,26 +28,43 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int pager=0x7f080000;
-        public static final int section_label=0x7f080003;
-        public static final int textView1=0x7f080002;
-        public static final int title=0x7f080001;
+        public static final int action_compose=0x7f09000e;
+        public static final int action_search=0x7f09000d;
+        public static final int action_settings=0x7f09000c;
+        public static final int drawer_layout=0x7f090000;
+        public static final int frame_container=0x7f090001;
+        public static final int icon=0x7f09000b;
+        public static final int imageView1=0x7f090006;
+        public static final int list1=0x7f090007;
+        public static final int list2=0x7f090008;
+        public static final int list3=0x7f090009;
+        public static final int pager=0x7f090003;
+        public static final int section_label=0x7f09000a;
+        public static final int slider_list=0x7f090002;
+        public static final int spinner1=0x7f090005;
+        public static final int title=0x7f090004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_a=0x7f030001;
-        public static final int fragment_b=0x7f030002;
-        public static final int fragment_c=0x7f030003;
-        public static final int fragment_main_dummy=0x7f030004;
+        public static final int activity_main1=0x7f030001;
+        public static final int fb_fragment=0x7f030002;
+        public static final int fragment_a=0x7f030003;
+        public static final int fragment_b=0x7f030004;
+        public static final int fragment_c=0x7f030005;
+        public static final int fragment_main_dummy=0x7f030006;
+        public static final int gp_fragment=0x7f030007;
+        public static final int list_item=0x7f030008;
+        public static final int tb_fragment=0x7f030009;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
+        public static final int menu1=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int imgdesc=0x7f050003;
     }
     public static final class style {
         /** 
@@ -67,10 +88,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
