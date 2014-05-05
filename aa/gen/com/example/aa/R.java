9a13d483d1fdc16d0e5d41529ef6634e30d1e52c
davide.scarano <davidescarano1991@gmail.com>
2014-05-05 20:20:37 +0200

    committ by ds

--- aa/gen/com/example/aa/R.java
+++ aa/gen/com/example/aa/R.java
@@ -28,25 +28,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_compose=0x7f090012;
-        public static final int action_search=0x7f090011;
-        public static final int action_settings=0x7f090010;
-        public static final int button1=0x7f090007;
-        public static final int button2=0x7f090008;
-        public static final int checkBox1=0x7f09000a;
+        public static final int action_compose=0x7f09000f;
+        public static final int action_search=0x7f09000e;
+        public static final int action_settings=0x7f09000d;
+        public static final int button1=0x7f09000b;
         public static final int drawer_layout=0x7f090000;
         public static final int frame_container=0x7f090001;
-        public static final int icon=0x7f09000f;
+        public static final int icon=0x7f09000c;
         public static final int imageView1=0x7f090006;
-        public static final int list1=0x7f09000b;
-        public static final int list2=0x7f09000c;
-        public static final int list3=0x7f09000d;
+        public static final int list1=0x7f090007;
+        public static final int list2=0x7f090008;
+        public static final int list3=0x7f090009;
         public static final int pager=0x7f090003;
-        public static final int section_label=0x7f09000e;
+        public static final int section_label=0x7f09000a;
         public static final int slider_list=0x7f090002;
         public static final int spinner1=0x7f090005;
         public static final int title=0x7f090004;
-        public static final int toggleButton1=0x7f090009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
