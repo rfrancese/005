783d391da754920fd8eb79e88e17b53d0092eb9f
ludimar <ludimar@192.168.1.67>
2014-06-27 15:43:27 +0200

    Primo tentativo di fix della struttura delle directory.

--- aa/src/com/example/aa/Contatta.java
+++
@@ -1,8 +0,0 @@
-package com.example.aa;
-
-import android.app.Fragment;
-import android.support.v4.app.ListFragment;
-
-public class Contatta extends ListFragment{
-
-}
--- aa/src/com/example/aa/calendario.java
+++
@@ -1,7 +0,0 @@
-package com.example.aa;
-
-import android.support.v4.app.ListFragment;
-
-public class calendario extends ListFragment {
-
-}
