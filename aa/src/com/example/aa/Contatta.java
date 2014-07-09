7ba39894595af34353a34a8182fd513368d66e42
davide.scarano <davidescarano1991@gmail.com>
2014-07-09 14:10:07 +0200

    FINAL COMMIT BY DS LDM & FBM 9/07

--- aa/src/com/example/aa/Contatta.java
+++ aa/src/com/example/aa/Contatta.java
@@ -1,8 +1,82 @@
 package com.example.aa;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Contatta extends ListFragment{
+	private TextView t1,t2,t3,t4;
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
+
+		View rootView = inflater.inflate(R.layout.contatta,container, false);
+		ListView list=(ListView) rootView.findViewById(android.R.id.list);
+
+		 t1 = (TextView)rootView.findViewById(R.id.da);
+		 t2 = (TextView)rootView.findViewById(R.id.fran);
+		 t3 = (TextView)rootView.findViewById(R.id.gi);
+		 t4 = (TextView)rootView.findViewById(R.id.sito);
+		 t1.setOnClickListener(new View.OnClickListener() {
+			    public void onClick(View v) {
+			    	Intent i = new Intent(Intent.ACTION_SEND);
+			    	i.setType("message/rfc822");
+			    	i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"davidescarano@gmail.com"});
+			    	i.putExtra(Intent.EXTRA_SUBJECT,"ASSISTENZA CLUBADVISOR");
+			    	i.putExtra(Intent.EXTRA_TEXT   , "");
+			    	try {
+			    	    startActivity(Intent.createChooser(i, "Invio in corso..."));
+			    	} catch (android.content.ActivityNotFoundException ex) {
+			    	    Toast.makeText(getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+			    	}
+			    }
+			});
+		 t2.setOnClickListener(new View.OnClickListener() {
+			    public void onClick(View v) {
+			    	Intent i = new Intent(Intent.ACTION_SEND);
+			    	i.setType("message/rfc822");
+			    	i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"franbarbosa90@gmail.com"});
+			    	i.putExtra(Intent.EXTRA_SUBJECT, "ASSISTENZA CLUBADVISOR");
+			    	i.putExtra(Intent.EXTRA_TEXT   , "");
+			    	try {
+			    	    startActivity(Intent.createChooser(i, "Invio in corso..."));
+			    	} catch (android.content.ActivityNotFoundException ex) {
+			    	    Toast.makeText(getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+			    	}
+			    }
+			});
+		 t3.setOnClickListener(new View.OnClickListener() {
+			    public void onClick(View v) {
+			    	Intent i = new Intent(Intent.ACTION_SEND);
+			    	i.setType("message/rfc822");
+			    	i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"dimartinoluigi@live.it"});
+			    	i.putExtra(Intent.EXTRA_SUBJECT, "ASSISTENZA CLUBADVISOR");
+			    	i.putExtra(Intent.EXTRA_TEXT   , "");
+			    	try {
+			    	    startActivity(Intent.createChooser(i, "Invio in corso..."));
+			    	} catch (android.content.ActivityNotFoundException ex) {
+			    	    Toast.makeText(getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+			    	}
+			    }
+			});
+		 t4.setOnClickListener(new View.OnClickListener() {
+			    public void onClick(View v) {
+			    	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ludimar.altervista.org/clubadvisor/html/index.html"));
+			    	startActivity(browserIntent);
+			    }
+			});
+		return rootView;
+	}
+	
+	public void onStart(){
+		super.onStart();
+		
+	}
 }
--- aa/src/com/example/aa/calendario.java
+++ aa/src/com/example/aa/calendario.java
@@ -1,7 +1,25 @@
 package com.example.aa;
 
+import java.util.Calendar;
+
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class calendario extends ListFragment {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+
+		View rootView = inflater.inflate(R.layout.calendario,container, false);
+		ListView list=(ListView) rootView.findViewById(android.R.id.list);
+		Toast.makeText(getActivity(), "QUESTA PAGINA NON é DISPONIBILE!" , 
+				   Toast.LENGTH_LONG).show();
+		return rootView;
+	}
 
 }
